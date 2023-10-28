d579d6e08eed1ef0bd3b8105335ecaf0fbf75bb5
Laurence Darnbrough-Brown <146700776+Ldb23@users.noreply.github.com>
2023-10-28 17:42:56 +0000

    Final assessment attempt added (completed success)

--- Assessment_Task_1/StepsTask1.c
+++ Assessment_Task_1/StepsTask1.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Define an appropriate struct
+// Definition of a struct
 typedef struct {
 	char date[11];
 	char time[6];
@@ -14,8 +14,11 @@ typedef struct {
 char date[11]; // Initialises an empty character array
 char time[6]; // Initialises an empty character array
 char steps[10];  // Initialises a large enough char array to hold a typical step count as a string
-int totalLines = 0; // Initialises a line count variable
+int buffer_size = 100; // Initialises a buffer size for lines in the csv file (100 more than sufficient for standard date,time,steps format)
+int lineCount = 0; // Initialises a line count variable
+int currentLine = 0; // Initialises a line tracking variable
 
+// Function defined to open file
 FILE *open_file(char filename[], char mode[]) {
     FILE *file = fopen(filename, mode);
     if (file == NULL) {
@@ -53,45 +56,50 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 }
 
-// Complete the main function
+// Main function
 int main() {
 
-    // Calls a function to read the fitness data
-    FILE *file = open_file("FitnessData_2023.csv", "r");
+    // Calls a function to read the fitness data. (To be able to count the number of lines)
+    FILE *file1 = open_file("FitnessData_2023.csv", "r");
 
-    // Size of container for the line you are reading
-    int buffer_size = 100;
-    
     // Stores line into containers of buffer size
     char line_buffer[buffer_size];
 
-    FITNESS_DATA allData[59];
+    // While lines have some text, increment the lineCount
+    while (fgets(line_buffer, buffer_size, file1) != NULL) {
+        lineCount += 1;
+    }
+    fclose(file1);
 
-    // While lines have some text, print each line
-    while (fgets(line_buffer, buffer_size, file) != NULL) {
+    // Now an array of structs is defined with size = the number of lines of fitness data
+    FITNESS_DATA allData[lineCount];
 
-        // Counts the number of lines in the csv file, and thus the required array size
-        totalLines += 1;
+    // Calls a function to read the fitness data. (To be able to populate allData)
+    FILE *file2 = open_file("FitnessData_2023.csv", "r");
+
+    while (fgets(line_buffer, buffer_size, file2) != NULL) {
+
+        // Keeps track of current line being read in the while loop
+        currentLine += 1;
         
-        // Splits the input (current line being read) by the , to output assignments to date, time and steps variables
+        // Splits the input (current line being read) by the , to output assignments to the globally defined date, time and steps variables
         tokeniseRecord(line_buffer, ",", date, time, steps);
 
-        
-        strcpy(allData[totalLines - 1].date, date); // Assigns the date in the current line, to the date in the allData struct at the current element
-        strcpy(allData[totalLines - 1].time, time); // Assigns the time in the current line, to the time in the allData struct at the current element
-        allData[totalLines - 1].steps = atoi(steps); // Assigns the converted integer equiv of steps in the current line, to the steps in the allData struct at the current element
+        strcpy(allData[currentLine - 1].date, date); // Assigns the date in the current line, to the date in the allData struct at the current element
+        strcpy(allData[currentLine - 1].time, time); // Assigns the time in the current line, to the time in the allData struct at the current element
+        allData[currentLine - 1].steps = atoi(steps); // Assigns the converted integer equiv of steps in the current line, to the steps in the allData struct at the current element
 
     }
     
-    fclose(file);
+    fclose(file2);
 
     // Prints the total number of records in file, in the specified format
-    printf("Number of records in file: %d\n", totalLines);
+    printf("Number of records in file: %d\n", lineCount);
 
     // Prints the first three lines of the file in the specified format of date/time/steps
     for (int i = 0; i < 3; i++){
         printf("%s/%s/%d\n", allData[i].date, allData[i].time, allData[i].steps);
     }
-    
 
+    return 0;
 }
